3a7a3bb338f7d16de8d724c421118e65141d8b46
HoussamBedja <houssam.b@hotmail.com>
2021-03-28 15:43:49 -0400

    build chat section and chat input

--- src/Chat.js
+++ src/Chat.js
@@ -1,7 +1,19 @@
-import React from 'react'
+import { IconButton } from '@material-ui/core';
+import { SendRounded } from '@material-ui/icons';
+import React, { useState } from 'react'
 import './Chat.css'
 
 export default function Chat() {
+
+  const [input, setInput] = useState('')
+
+  const sendMessage = (e) => {
+    e.preventDefault();
+
+    setInput('');
+
+  };
+
   return (
     <div className="chat">
       <div className="chat__header">
@@ -10,8 +22,27 @@ export default function Chat() {
         </h4>
         <strong>Details</strong>
       </div>
-      {/* chat messages */}
+
+      <div className="chat__messages">
+        <h2>Messgae here</h2>
+      </div>
+
       {/* chat input */}
+      <div className="chat__input">
+        <form>
+          <input
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+            placeholder="Message"
+            type="text"
+          />
+          <button onClick={sendMessage}>Send Message</button>
+        </form>
+        
+        <IconButton>
+          <SendRounded />
+        </IconButton>
+      </div>
     </div>
   );
 }
